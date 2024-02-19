2a094d1a1dfce8a714214236615ac1dbeaaf3294
AniviaT05 <augustthomsen05@gmail.com>
2024-02-19 15:28:33 +0100

    Movement og usynlige barierre

--- Mogging-main/Mogging/Assets/Classes/Player/PlayerMovement.cs
+++ Mogging-main/Mogging/Assets/Classes/Player/PlayerMovement.cs
@@ -4,15 +4,51 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    private Vector3 Velocity;
+    private Vector3 PlayerMovementInput;
+    private Vector2 PlayerMouseInput;
+    private float xRot;
+
+    [SerializeField] private Transform PlayerCamera;
+    [SerializeField] private CharacterController Controller;
+    [Space]
+    [SerializeField] private float Speed;
+    [SerializeField] private float Sensitivity;
+
+    private void Update()
     {
-        
+        PlayerMovementInput = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical"));
+        PlayerMouseInput = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
+
+        MovePlayer();
+        MovePlayerCamera();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void MovePlayer()
     {
-        
+        Vector3 MoveVector = transform.TransformDirection(PlayerMovementInput);
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            Velocity.y = 1f;
+        }
+        else if (Input.GetKey(KeyCode.LeftShift))
+        {
+            Velocity.y = -1f;
+        }
+
+        Controller.Move(MoveVector * Speed * Time.deltaTime);
+        Controller.Move(Velocity * Speed * Time.deltaTime);
+
+        Velocity.y = 0f;
     }
+
+    private void MovePlayerCamera()
+    {
+        xRot -= PlayerMouseInput.y * Sensitivity;
+        transform.Rotate(0f, PlayerMouseInput.x * Sensitivity, 0f);
+        PlayerCamera.transform.localRotation = Quaternion.Euler(xRot, 0f, 0f);
+    }
+      
 }
