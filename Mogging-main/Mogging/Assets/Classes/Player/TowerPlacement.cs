d31dd359b2321f534a81d86219ab3d9438464593
StivDolkUnderBord <ninjakai284@gmail.com>
2024-02-28 13:19:58 +0100

    abemand

--- Mogging-main/Mogging/Assets/Classes/Player/TowerPlacement.cs
+++ Mogging-main/Mogging/Assets/Classes/Player/TowerPlacement.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class TowerPlacement : MonoBehaviour
 {
-
+    [SerializeField] private LayerMask PlacementCheckMask;
+    [SerializeField] private LayerMask PlacementCollideMask;
     [SerializeField] private Camera PlayerCamera;
 
     private GameObject CurrentPlacingTower;
@@ -22,13 +24,31 @@ public class TowerPlacement : MonoBehaviour
         {
 
             Ray camray = PlayerCamera.ScreenPointToRay(Input.mousePosition);
-            if(Physics.Raycast(camray, out RaycastHit hitInfo, 100f))
+            RaycastHit HitInfo;
+            if(Physics.Raycast(camray, out HitInfo, 100f, PlacementCollideMask))
             {
-                CurrentPlacingTower.transform.position = hitInfo.point;
+                CurrentPlacingTower.transform.position = HitInfo.point;
             }
 
-            if(Input.GetMouseButtonDown(0))
+            if(Input.GetMouseButtonDown(0) && HitInfo.collider.gameObject != null)
             {
+                if(!HitInfo.collider.gameObject.CompareTag("CantPlace"))
+                {
+                    BoxCollider TowerCollider =  CurrentPlacingTower.gameObject.GetComponent<BoxCollider>();
+                    TowerCollider.isTrigger = true;
+
+                    Vector3 BoxCenter = CurrentPlacingTower.gameObject.transform.position + TowerCollider.center;
+                    Vector3 HalfExtents = TowerCollider.size / 2;
+                    if(!Physics.CheckBox(BoxCenter, HalfExtents, Quaternion.identity, PlacementCheckMask, QueryTriggerInteraction.Ignore))
+                    {
+                        TowerCollider.isTrigger = false;
+                        CurrentPlacingTower = null;
+                    }
+                }
+                
+                
+                
+                
                 CurrentPlacingTower = null;
             }
                     
